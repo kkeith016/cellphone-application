ebfc92d2189d3728a43875445ab0fefe5ed0a81e
Kylee <kkeith.biz@gmail.com>
2025-09-30 15:46:17 -0400

    Setters finished - moving on to the main app

--- src/main/java/com/pluralsight/Cellphone.java
+++ src/main/java/com/pluralsight/Cellphone.java
@@ -34,6 +34,21 @@ class CellPhone {
     public String getOwner() {
         return owner;
     }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
 
 
