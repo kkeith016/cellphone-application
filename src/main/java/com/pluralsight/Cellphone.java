a03c907d9b96f4c97bbcc0cecf7484621962a0cd
Kylee <kkeith.biz@gmail.com>
2025-09-30 15:41:59 -0400

    Getters

--- src/main/java/com/pluralsight/Cellphone.java
+++ src/main/java/com/pluralsight/Cellphone.java
@@ -4,20 +4,22 @@ class CellPhone {
 
 
     // Data members
-    private int serialNumber;
+    private String serialNumber;
     private String model;
     private String carrier;
     private String phoneNumber;
     private String owner;
 
     // Parameterless constructor (default values)
-    public CellPhone() {
+    public CellPhone(Sting serialnumber,Sting model,String carrier,String phoneNumber,String owner) {
         this.serialNumber = 0;
         this.model = "";
         this.carrier = "";
         this.phoneNumber = "";
         this.owner = "";
     }
+    //Getters
 }
+
 
 
