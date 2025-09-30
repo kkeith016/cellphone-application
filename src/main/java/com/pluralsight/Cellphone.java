b0eb7b4939c62e99a5c3857a43444fc67341c6b2
Kylee <kkeith.biz@gmail.com>
2025-09-30 15:37:22 -0400

    initial commit whoops

---
+++ src/main/java/com/pluralsight/Cellphone.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+class CellPhone {
+
+
+    // Data members
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    // Parameterless constructor (default values)
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+}
+
+
