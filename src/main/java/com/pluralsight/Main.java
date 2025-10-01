389b6d23d40d4751468307a1be5c0cdf7c177e32
Kylee <kkeith.biz@gmail.com>
2025-10-01 09:38:26 -0400

    added the scanner and prints

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter your phones serial number: ");
+        String serialNumber = scan.nextLine();
+
+        System.out.print("What model is the phone? ");
+        String model = scan.nextLine();
+
+        System.out.print("What is the carrier? ");
+        String carrier = scan.nextLine();
+
+        System.out.print("What is the phone number? ");
+        String phoneNumber = scan.nextLine();
+
+        System.out.print("Who is the owner? ");
+        String owner = scan.nextLine();
+    }
 }
