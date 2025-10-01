3e810e393be5c1af0aee2a812a16ebb5190533f2
Kylee <kkeith.biz@gmail.com>
2025-10-01 13:48:03 -0400

    updated application with dial

--- src/main/java/com/pluralsight/Cellphone.java
+++ src/main/java/com/pluralsight/Cellphone.java
@@ -3,54 +3,59 @@ package com.pluralsight;
 class CellPhone {
 
 
-    // Data members
-    private String serialNumber;
-    private String model;
-    private String carrier;
-    private String phoneNumber;
-    private String owner;
+    private int serialNumber;
+    private String model, carrier, phoneNumber, owner;
 
-    // Parameterless constructor (default values)
-    public CellPhone() {
-        this.serialNumber = "";
+    public CellPhone() { //parameterless
+        this.serialNumber = 0;
         this.model = "";
         this.carrier = "";
         this.phoneNumber = "";
         this.owner = "";
     }
 
-
-    //Getters
-    public String getSerialNumber() {
-        return serialNumber;
+    public void dial(String phoneNumber) {
+        System.out.println(getOwner() + "'s phone is calling " + phoneNumber);
     }
+
+    public int getSerialNumber() {
+        return this.serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
     public String getModel() {
         return model;
     }
-    public String getCarrier() {
-        return carrier;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    public String getOwner() {
-        return owner;
-    }
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
+
     public void setModel(String model) {
         this.model = model;
     }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
     public void setCarrier(String carrier) {
         this.carrier = carrier;
     }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    public String getOwner() {
+        return owner;
+    }
+
     public void setOwner(String owner) {
         this.owner = owner;
-
     }
 }
 
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,31 +6,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        //Creating an object
-        CellPhone phone = new CellPhone();
+        CellPhone userPhoneOne = new CellPhone();
+        CellPhone userPhoneTwo = new CellPhone();
 
-        System.out.print("Please enter your phones serial number: ");
-        phone.setSerialNumber(scan.nextLine());
+        userPhoneTwo.setSerialNumber(888);
+        userPhoneTwo.setPhoneNumber("711");
+        userPhoneTwo.setOwner("Help Line");
+        userPhoneTwo.setCarrier("T-Mobile");
+        userPhoneTwo.setModel("Landline");
+
+        System.out.print("What is the serial number? ");
+        int serialNumber = scan.nextInt(); //looking for number
+        scan.nextLine(); //Looks for \n character
+        userPhoneOne.setSerialNumber(serialNumber);
 
         System.out.print("What model is the phone? ");
-        phone.setModel(scan.nextLine());
+        String model = scan.nextLine();
+        userPhoneOne.setModel(model);
 
-        System.out.print("What is the carrier? ");
-        phone.setCarrier(scan.nextLine());
+        System.out.print("Who is the carrier? ");
+        String carrier = scan.nextLine();
+        userPhoneOne.setCarrier(carrier);
 
         System.out.print("What is the phone number? ");
-        phone.setPhoneNumber(scan.nextLine());
+        String phoneNumber = scan.nextLine();
+        userPhoneOne.setPhoneNumber(phoneNumber);
 
         System.out.print("Who is the owner? ");
-        phone.setOwner(scan.nextLine());
+        String owner = scan.nextLine();
+        userPhoneOne.setOwner(owner);
 
-        //printing using getters
-        System.out.println("\n=== Phone Information ===");
+//        display(userPhoneOne);
+//        display(userPhoneTwo);
+
+
+        userPhoneOne.dial("(999) 555-1202");
+
+    }
+
+    public static void display(CellPhone phone) {
         System.out.println("Serial Number: " + phone.getSerialNumber());
         System.out.println("Model: " + phone.getModel());
         System.out.println("Carrier: " + phone.getCarrier());
         System.out.println("Phone Number: " + phone.getPhoneNumber());
         System.out.println("Owner: " + phone.getOwner());
-
     }
+
 }
